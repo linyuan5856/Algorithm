37f17e51f97aab10126009d851c64c394af1fc21
JimmyLIn <linyuan5856139@163.com>
2018-07-22 18:38:23 +0800

    first commit

---
+++ Algrothim_4th/src/Main.java
@@ -0,0 +1,9 @@
+import edu.princeton.cs.algs4.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+         StdDraw.line(10.0f,20.0f,0.0f,10.0f);
+    }
+}
