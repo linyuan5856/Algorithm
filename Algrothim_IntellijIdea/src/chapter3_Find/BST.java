c1c8a439ca8b30e4d1b4a0748493197e6755f990
JimmyLIn <linyuan5856139@163.com>
2018-08-07 00:55:16 +0800

    Update BST

--- Algrothim_IntellijIdea/src/chapter3_Find/BST.java
+++ Algrothim_IntellijIdea/src/chapter3_Find/BST.java
@@ -78,5 +78,60 @@ public class BST<Key extends Comparable<Key>, Value> {
         else return x;
     }
 
+    public Key Select(int k) {
+        return select(root, k).key;
+    }
 
+
+    private Node select(Node x, int k) {
+        if (x == null) return null;
+        int t = size(x.left);
+        if (t > k) return select(x.left, k);
+        else if (t < k) return select(x.right, k - t - 1);
+        else return x;
+    }
+
+    public int rank(Key key) {
+        return rank(key, root);
+    }
+
+    private int rank(Key key, Node x) {
+        if (x == null) return 0;
+        int camp = key.compareTo(x.key);
+        if (camp < 0) return rank(key, x.left);
+        else if (camp > 0) return 1 + size(x.left) + rank(key, x.right);
+        else return size(x.left);
+    }
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void delete(Key key) {
+        delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.right == null) return x.left;
+            if (x.left == null) return x.right;
+            Node t = x;
+            x = min(x.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
 }
